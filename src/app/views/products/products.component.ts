609afc9ccc537a887c9b871fd14dbd28b7a063f8
Ahmed Abdo <devahmedabdo@gmail.com>
2023-03-11 14:54:50 +0200

    add files

--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,5 +1,6 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-products',
@@ -18,5 +19,33 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
   constructor() {}
   show: boolean = true;
+  divs: any = [
+    {
+      name: 'WEB DEVELOPMENT',
+      icon: 'assets/web.svg',
+      dis: `Build a website that is as unique as your business with our expert web development services.Let's create an online presence that stands out.`,
+    },
+    {
+      name: 'SOCIAL MEDIA MARKETING',
+      icon: 'assets/meta.svg',
+      dis: `Get your business in front of the right people with
+      our targeted Meta Ads on Facebook and Instagram. 
+      Let's reach your ideal customers Now.`,
+    },
+    {
+      name: 'LINKEDIN ADS',
+      icon: 'assets/linkedin.svg',
+      dis: `Connect with top-notch clients and unlock 
+      endless opportunities with our LinkedIn Ads. 
+      Let's take your business to new heights!`,
+    },
+    {
+      name: 'SEO SOLUTIONS',
+      icon: 'assets/seo.svg',
+      dis: `Boost your online visibility and attract more 
+      customers with our customized SEO solutions. 
+      Let's unlock your business's full potential!`,
+    },
+  ];
   ngOnInit(): void {}
 }
